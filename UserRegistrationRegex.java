94a4c2bcffb67a517c0883ed08727e0c9e8f4451
mrunal08 <mrunalg96@gmail.com>
2021-09-07 10:30:50 +0530

    User Registration Program UC2

--- UserRegistrationRegex.java
+++ UserRegistrationRegex.java
@@ -5,5 +5,15 @@ public class UserRegistrationRegex {
 
         System.out.println("Welcome to User Registration Program Using Regex!!!");
 
+
+        Scanner scan = new Scanner(System.in);
+
+        String uc1Regex= "^[A-Z]{1}[a-zA-Z]{2,20}";
+        System.out.println("Enter Last Name with minimum three alphabets!!!");
+        String name = scan.next();
+        System.out.println("Is the input last name valid ? : "+name.matches(uc1Regex));
+
+
     }
 }
+
