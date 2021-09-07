845780c8eadfa76dd885c9736ff36680895a53dd
mrunal08 <mrunalg96@gmail.com>
2021-09-07 10:24:04 +0530

    User Registration Program Using Regex

---
+++ UserRegistrationRegex.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class UserRegistrationRegex {
+    public static void main(String[] args){
+
+        System.out.println("Welcome to User Registration Program Using Regex!!!");
+
+    }
+}
