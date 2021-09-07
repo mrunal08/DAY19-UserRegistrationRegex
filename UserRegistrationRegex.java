7b2b98ea1a808e35f406dd2e5f720386587864e4
mrunal08 <mrunalg96@gmail.com>
2021-09-07 17:31:38 +0530

    User Registration Program UC5-password with Min 8 characters

--- UserRegistrationRegex.java
+++ UserRegistrationRegex.java
@@ -5,5 +5,13 @@ public class UserRegistrationRegex {
 
         System.out.println("Welcome to User Registration Program Using Regex!!!");
 
+        Scanner scan = new Scanner(System.in);
+
+        String passwordRegex= "[!\"#$%&'()*+,-./:;<=>?@\\^_`{|}~A-Za-z0-9]{8,15}+";
+        System.out.println("Enter the password with minimum 8 characters!!!");
+        String password = scan.next();
+        System.out.println("Is the password valid ? : "+password.matches(passwordRegex));
+
+
     }
 }
